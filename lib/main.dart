ff5372867de780d6b7934245e97790fd98b2dcc5
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2024-04-28 17:19:28 +0300

    Store current language in secure storage.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
+import 'package:pic_translate/enums/enums.dart';
+import 'package:pic_translate/enums/languages.dart';
 import 'package:pic_translate/models/models.dart';
 import 'package:pic_translate/screens/screens.dart';
 
 final getIt = GetIt.instance;
 
-void setupGetIt() {
-  getIt.registerSingleton<AppData>(AppData());
+Future<void> setupGetIt() async {
+  getIt.registerSingleton<AppData>(
+      AppData(currentLanguage: await currentLanguage()));
 }
 
-void main() {
-  setupGetIt();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupGetIt();
   runApp(const MyApp());
 }
 
 final appData = getIt<AppData>();
+Future<Languages> currentLanguage() async {
+  const storage = FlutterSecureStorage();
+  final currentLanguage = await storage.read(key: 'currentLanguage');
+  if (currentLanguage == null) {
+    await storage.write(key: 'currentLanguage', value: Languages.Arabic.name);
+    return Languages.Arabic;
+  }
+  return languesFromString(currentLanguage);
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
